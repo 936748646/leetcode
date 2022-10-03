f27f35568d2be00ca30340e925c54fecbc87a3d4
Qiaoyun Liu <47050431+936748646@users.noreply.github.com>
2022-10-03 20:48:29 +0800

    Create 121_买卖股票的最佳时机.py

---
+++ 精选200题/算法思想/贪心思想/121_买卖股票的最佳时机.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # 贪心：只要找到当前节点之前的最小价格买入，在当前价格卖出，看当前是否是最大收益
+        # 动态规划方法见初中级算法
+        min_val = prices[0]
+        max_profix = 0
+        for i in range(1, len(prices)):
+            min_val = min(prices[i], min_val)
+            max_profix = max(prices[i] - min_val, max_profix)
+        return max_profix
