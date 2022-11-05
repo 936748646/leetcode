ebe71cb3814b39f72b3a5d5ec121bdbe55c9bc9a
Qiaoyun Liu <47050431+936748646@users.noreply.github.com>
2022-11-05 15:04:08 +0800

    Create 278_第一个错误的版本.py

---
+++ 精选200题/算法思想/二分查找/278_第一个错误的版本.py
@@ -0,0 +1,11 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        l, h = 1, n
+        while(l < h):
+            m = l + (h - l) // 2
+            if isBadVersion(m): h = m
+            else: l = m + 1
+        return l
